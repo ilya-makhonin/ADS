fcc1af3a879ae7aa34733846d7fa7e0416cd9607
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-24 01:15:32 +0300

    Created class for making Dequeue Data Structure

---
+++ src/DataStructures/Dequeue/index.js
@@ -0,0 +1,87 @@
+class Dequeue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this._count = 0;
+        this.NodeCreator = require('../LinkedList/DoubleLinkedList/doubleNode');
+    }
+
+    PushBack(value) {
+        let newNode = new this.NodeCreator(value);
+
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.Next = newNode;
+            newNode.Previous = this.tail;
+            this.tail = newNode;
+        }
+
+        this._count++;
+    }
+
+    PushFront(value) {
+        let newNode = new this.NodeCreator(value);
+
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.Previous = newNode;
+            newNode.Next = this.head;
+            this.head = newNode;
+        }
+
+        this._count++;
+    }
+
+    PopBack() {
+        if (this._count) {
+            if (this._count === 1) {
+                let result = this.tail.Value;
+                this.Clear();
+                return result;
+            } else {
+                let result = this.tail.Value;
+                this.tail.Previous.Next = null;
+                this.tail = this.tail.Previous;
+                this._count--;
+                return result;
+            }
+        }
+    }
+
+    PopFront() {
+        if (this._count) {
+            if (this._count === 1) {
+                let result = this.head.Value;
+                this.Clear();
+                return result;
+            } else {
+                let result = this.head.Value;
+                this.head.Next.Previous = null;
+                this.head = this.head.Next;
+                this._count--;
+                return result;
+            }
+        }
+    }
+
+    Clear() {
+        this.head = null;
+        this.tail = null;
+        this._count = 0;
+    }
+
+    IsEmpty() {
+		return this._count ? false : true;
+	}
+
+    get Lenght() {
+        return this._count;
+    }
+}
+
+
+module.exports = Dequeue;
