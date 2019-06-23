e1ab0e179bc994819553238f7e1ef49aa01703a2
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-23 23:46:59 +0300

    Created the file for grouping imports from Data Structures folder

---
+++ src/DataStructures/all.js
@@ -0,0 +1,13 @@
+const Stack = require('./Stack');
+const Set = require('./Set');
+const Queue = require('./Queue');
+const { LinkedList, DoubleLinkedList } = require('./LinkedList');
+
+
+module.exports.allDataStructures = {
+    Stack,
+    Set,
+    Queue,
+    LinkedList,
+    DoubleLinkedList
+};
