8bff74453b59130a33e58d228ecd2f5660d8c093
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-29 23:50:56 +0300

    Updated the file

--- src/Algorithms/all.js
+++ src/Algorithms/all.js
@@ -1,11 +1,13 @@
 const bubbleSort = require('./Sort/Bubble');
 const selectionSort = require('./Sort/Selection');
-const {mergeSort, mergeSortLazy} = require('./Sort/Merge');
+const { mergeSort, mergeSortLazy } = require('./Sort/Merge');
+const insertionSort = require('./Sort/Insertion');
 
 
 module.exports.allAldorithms = {
     bubbleSort,
     selectionSort,
     mergeSort,
-    mergeSortLazy
+    mergeSortLazy,
+    insertionSort
 };
