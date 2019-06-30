f59d1336af306f6f525440851f3b9621b90a89ff
Ilia Makhonin <noizemkutane77@gmail.com>
2019-06-30 22:46:00 +0300

    Updated an entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const { allDataStructures } = require('./DataStructures/all');
+const { allAldorithms } = require('./Algorithms/all');
 
 
-const forExports = Object.assign({}, allDataStructures);
+const forExports = Object.assign({}, allDataStructures, allAldorithms);
 
 
 module.exports.ADS = forExports;
