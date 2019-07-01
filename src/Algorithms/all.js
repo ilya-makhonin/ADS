3e5a21eee97d771a69c255f0301c863af3583ee1
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-01 22:50:01 +0300

    Updated the file

--- src/Algorithms/all.js
+++ src/Algorithms/all.js
@@ -1,16 +1,7 @@
-const bubbleSort = require('./Sort/Bubble');
-const selectionSort = require('./Sort/Selection');
-const { mergeSort, mergeSortLazy } = require('./Sort/Merge');
-const { insertionSort, insertionSortReverse } = require('./Sort/Insertion');
-const shellSort = require('./Sort/Shell');
+const { sortAlgorithms } = require('./Sort/sortAlgorithms');
 
 
-module.exports.allAldorithms = {
-    bubbleSort,
-    selectionSort,
-    mergeSort,
-    mergeSortLazy,
-    insertionSort,
-    insertionSortReverse,
-    shellSort
-};
+const algorithms = Object.assign({}, sortAlgorithms);
+
+
+module.exports.allAldorithms = algorithms;
