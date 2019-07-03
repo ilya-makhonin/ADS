4191457e977d884cd613bc90c8952d876b692e98
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-03 23:32:52 +0300

    Changed exporting of Algorithms and Data Structures

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const { allDataStructures } = require('./DataStructures/all');
-const { allAldorithms } = require('./Algorithms/all');
+const DataStructures = require('./DataStructures/all');
+const Algorithms = require('./Algorithms/all');
 
 
-const forExports = Object.assign({}, allDataStructures, allAldorithms);
+const forExports = Object.assign({}, DataStructures, Algorithms);
 
 
 module.exports.ADS = forExports;
