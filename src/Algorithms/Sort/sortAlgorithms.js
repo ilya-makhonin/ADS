cc418e537da5ab18d86c0a422bc3e72f4cf1cff4
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-03 23:30:35 +0300

    Updated the file

--- src/Algorithms/Sort/sortAlgorithms.js
+++ src/Algorithms/Sort/sortAlgorithms.js
@@ -1,6 +1,6 @@
 const bubbleSort = require('./Bubble');
 const selectionSort = require('./Selection');
-const { mergeSort, mergeSortLazy } = require('./Merge');
+const { mergeSortInPlace, mergeSortOutPlace } = require('./Merge');
 const { insertionSort, insertionSortReverse } = require('./Insertion');
 const shellSort = require('./Shell');
 const { quickSortLow, quickSortHight } = require('./Quick');
@@ -10,8 +10,8 @@ const countingSort = require('./Counting');
 module.exports.sortAlgorithms = {
     bubbleSort,
     selectionSort,
-    mergeSort,
-    mergeSortLazy,
+    mergeSortInPlace,
+    mergeSortOutPlace,
     insertionSort,
     insertionSortReverse,
     shellSort,
