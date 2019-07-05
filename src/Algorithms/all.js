0d4e233260440832ed56161ab2171019d9847efb
Ilia Makhonin <noizemkutane77@gmail.com>
2019-07-05 23:29:43 +0300

    Updated the file

--- src/Algorithms/all.js
+++ src/Algorithms/all.js
@@ -1,7 +1,8 @@
 const Sort = require('./Sort/sortAlgorithms');
+const Search = require('./Search/searchAlgorithms');
 
 
-const algorithms = Object.assign({}, Sort);
+const algorithms = Object.assign({}, Sort, Search);
 
 
 module.exports.Algorithms = algorithms;
